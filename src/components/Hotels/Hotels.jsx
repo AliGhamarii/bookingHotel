9c72d45ff5e6284743e1dbaa06d4c90476490d82
aliGhamari <ali2qamari@gmail.com>
2025-09-16 14:04:13 +0330

    change center of map

--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -7,12 +7,12 @@ function Hotels() {
   if (isLoading) return <Loading />;
 
   return (
-    <div className="h-[750px] overflow-y-auto">
+    <div className="h-[700px] overflow-y-auto">
       <h1 className="text-2xl font-bold mb-8 text-gray-800">
-        Your Searches Result ({hotels.length})
+        Your Search Results ({hotels.length})
       </h1>
 
-      <div className="flex flex-col gap-6 px-5">
+      <div className="flex flex-col gap-6 px-4">
         {hotels.map((hotel) => (
           <Link
             key={hotel.id}
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,33 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useHotels from "../../hooks/useHotels";
 import Loading from "../Loading/Loading";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useSearchParams } from "react-router-dom";
+import ChangeCenter from "./ChangeCenter";
 
 function Map() {
   const [mapCenter, setMapCenter] = useState([51.505, -0.09]);
   const { hotels, isLoading } = useHotels();
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
 
   const lat = searchParams.get("lat");
-  const lng = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  const isSingleHotel = lat && lng;
+
+  useEffect(() => {
+    if (lat && lng) setMapCenter([Number(lat), Number(lng)]);
+  }, [lat, lng]);
 
   if (isLoading) return <Loading />;
 
   return (
-    <div className="w-2/3 min-h-screen">
+    <div className="w-[60%] min-h-screen">
       <MapContainer
         center={mapCenter}
-        zoom={4}
+        zoom={isSingleHotel ? 15 : 4}
         scrollWheelZoom={true}
-        className="h-[650px] w-full rounded-lg shadow"
+        className="h-[700px] w-full rounded-lg shadow"
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
+        <ChangeCenter position={mapCenter} />
         {hotels.map((hotel) => (
           <Marker key={hotel.id} position={[hotel.latitude, hotel.longitude]}>
             <Popup>{hotel.host_location}</Popup>
